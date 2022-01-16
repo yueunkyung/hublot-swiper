ba9d5351547bdb115cfa1c3a4b92320127acf569
EunkyungYu <eunkyung211@naver.com>
2022-01-16 17:50:26 +0900

    swiper 이벤트 gsap 효과 추가

--- js/main.js
+++ js/main.js
@@ -19,18 +19,20 @@ function loadClock(_url) {
             const total = clockList.length;
             $.each(clockList, function (i, item) {
                 output += ` <li class="swiper-slide section" id="clock${addZero(i)}" data-splitting>
-                                <div class="img"><img src ="${item.bg}"></div>
-                                <div class="info">
-                                    <p class="category">${item.category}</p>
-                                    <p class="title">
-                                        ${item.title}
-                                    </p>
-                                    <p class="depth">
-                                        ${item.depth}MM
-                                    </p>
-                                    <p class="price">
-                                        CHF ${item.price}
-                                    </p>
+                                <div class="inner" data-swiper-parallax-scale="0.75">
+                                    <div class="img"><img src ="${item.bg}"></div>
+                                    <div class="info">
+                                        <p class="category">${item.category}</p>
+                                        <p class="title">
+                                            ${item.title}
+                                        </p>
+                                        <p class="depth">
+                                            ${item.depth}MM
+                                        </p>
+                                        <p class="price">
+                                            CHF ${item.price}
+                                        </p>
+                                    </div>
                                 </div>
                             </li>`;
             });
@@ -42,6 +44,30 @@ function loadClock(_url) {
                 slidesPerView: 1,
                 speed: 1000,
                 mousewheel: true,
+                parallax: true,
+                on: {
+                    slideChangeTransitionStart: function () {
+                        // console.log(this);
+                        $("#indicator .num").text(`${addZero(this.activeIndex + 1)}/${total}`);
+                        gsap.set(`.section:nth-child(${this.activeIndex + 1}) .char`, {
+                            y: -200,
+                            opacity: 0,
+                        });
+                    },
+                    slideChangeTransitionEnd: function () {
+                        // console.log(this);
+                        gsap.to(`.section:nth-child(${this.activeIndex + 1}) .char`, {
+                            y: 0,
+                            opacity: 1,
+                            duration: 1.5,
+                            ease: "bounce",
+                            stagger: {
+                                amount: 0.1,
+                                from: "random",
+                            },
+                        });
+                    },
+                },
             });
         },
     });
