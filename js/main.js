a7851681453bcb68faa138a6fb45ffe52e902718
EunkyungYu <eunkyung211@naver.com>
2022-01-16 17:28:24 +0900

    init

---
+++ js/main.js
@@ -0,0 +1,50 @@
+function addZero(num) {
+    return num < 10 ? "0" + num : num;
+}
+loadClock("../data/bigbang.json");
+$("#gnb .gnbList li").on("click", function () {
+    const url = $(this).data("url");
+    $(this).addClass("on").siblings("li").removeClass("on");
+    loadClock(`../data/${url}.json`);
+    return false;
+});
+function loadClock(_url) {
+    $.ajax({
+        url: _url,
+        success: function (res) {
+            // console.log("res", res);
+            const clockList = [...res.clock];
+            // console.log("clockList", clockList);
+            let output = "";
+            const total = clockList.length;
+            $.each(clockList, function (i, item) {
+                output += ` <li class="swiper-slide section" id="clock${addZero(i)}" data-splitting>
+                                <div class="img"><img src ="${item.bg}"></div>
+                                <div class="info">
+                                    <p class="category">${item.category}</p>
+                                    <p class="title">
+                                        ${item.title}
+                                    </p>
+                                    <p class="depth">
+                                        ${item.depth}MM
+                                    </p>
+                                    <p class="price">
+                                        CHF ${item.price}
+                                    </p>
+                                </div>
+                            </li>`;
+            });
+            $("#main .mask ul").html(output);
+            Splitting();
+            $("#indicator .num").text(`${addZero(1)}/${total}`);
+            new Swiper("#main .mask", {
+                direction: "vertical",
+                slidesPerView: 1,
+                speed: 1000,
+                mousewheel: true,
+            });
+        },
+    });
+}
+
+particlesJS.load("bg", "../data/particlesjs-config.json");
